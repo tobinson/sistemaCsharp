eded5d82ffbfdd852e8359201e625cb5d8f18450
Pc <coli@tobynson.com>
2019-04-23 08:26:01 -0500

    dpresentacion datos y negocio

---
+++ Datos/DPresentacion.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//usings necesarios para trabajar con sql
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Datos
+{
+    //hacerlo public para acceder desde negocio
+    public class DPresentacion
+    {
+        private int _Idpresentacion;
+        private string _Nombre;
+        private string _Descripcion;
+
+        private string _TextoBuscar;
+
+        public int Idpresentacion { get => _Idpresentacion; set => _Idpresentacion = value; }
+        public string Nombre { get => _Nombre; set => _Nombre = value; }
+        public string Descripcion { get => _Descripcion; set => _Descripcion = value; }
+        public string TextoBuscar { get => _TextoBuscar; set => _TextoBuscar = value; }
+
+        public DPresentacion()
+        {
+        }
+
+        public DPresentacion(int idpresentacion, string nombre, string descripcion, string textoBuscar)
+        {
+            this.Idpresentacion = idpresentacion;
+            this.Nombre = nombre;
+            this.Descripcion = descripcion;
+            this.TextoBuscar = textoBuscar;
+        }
+        //Metodo Insertar
+        public string Insertar(DPresentacion Presentacion)
+        {
+            string rpta = "";
+            SqlConnection sqlcon = new SqlConnection();
+            try
+            {
+                //establecer la cadena de conexion y abrirla
+                sqlcon.ConnectionString = Conexion.cn;
+                sqlcon.Open();
+                //establecer el comando para ejecutar sentecias sql
+                SqlCommand sqlcmd = new SqlCommand();
+                sqlcmd.Connection = sqlcon;
+                sqlcmd.CommandText = "spinsertar_presentacion";
+                sqlcmd.CommandType = CommandType.StoredProcedure;
+
+                //declarar parametros q recibe el procedimiento almacenado
+                SqlParameter parIdpresentacion = new SqlParameter();
+                parIdpresentacion.ParameterName = "@idpresentacion";
+                parIdpresentacion.SqlDbType = SqlDbType.Int;
+                //parametro de salida por ser autonumerico
+                parIdpresentacion.Direction = ParameterDirection.Output;
+                sqlcmd.Parameters.Add(parIdpresentacion);
+                //nombre
+                SqlParameter parNombre = new SqlParameter();
+                parNombre.ParameterName = "@nombre";
+                parNombre.SqlDbType = SqlDbType.VarChar;
+                parNombre.Size = 50;
+                //metodo get obtiene el metodo Nombre
+                parNombre.Value = Presentacion.Nombre;
+                sqlcmd.Parameters.Add(parNombre);
+                //descripcion
+                SqlParameter parDescripcion = new SqlParameter();
+                parDescripcion.ParameterName = "@descripcion";
+                parDescripcion.SqlDbType = SqlDbType.VarChar;
+                parDescripcion.Size = 256;
+                //metodo get obtiene el metodo Descrpcion
+                parDescripcion.Value = Presentacion.Descripcion;
+                sqlcmd.Parameters.Add(parDescripcion);
+
+                //ejecutamos nuestro comando
+                rpta = sqlcmd.ExecuteNonQuery() == 1 ? "Ok" : "No se ingreso el registro";
+            }
+            catch (Exception ex)
+            {
+                rpta = ex.Message;
+            }
+            finally
+            {
+                if (sqlcon.State == ConnectionState.Open) sqlcon.Close();
+            }
+            return rpta;
+        }
+        //Metodo Editar
+        public string Editar(DPresentacion Presentacion)
+        {
+            string rpta = "";
+            SqlConnection sqlcon = new SqlConnection();
+            try
+            {
+                //establecer la cadena de conexion y abrirla
+                sqlcon.ConnectionString = Conexion.cn;
+                sqlcon.Open();
+                //establecer el comando para ejecutar sentecias sql
+                SqlCommand sqlcmd = new SqlCommand();
+                sqlcmd.Connection = sqlcon;
+                sqlcmd.CommandText = "speditar_presentacion";
+                sqlcmd.CommandType = CommandType.StoredProcedure;
+
+                //declarar parametros q recibe el procedimiento almacenado
+                SqlParameter parIdpresentacion = new SqlParameter();
+                parIdpresentacion.ParameterName = "@idpresentacion";
+                parIdpresentacion.SqlDbType = SqlDbType.Int;
+                //parametro de entrada necesario para editar
+                parIdpresentacion.Value = Presentacion.Idpresentacion;
+                sqlcmd.Parameters.Add(parIdpresentacion);
+                //nombre
+                SqlParameter parNombre = new SqlParameter();
+                parNombre.ParameterName = "@nombre";
+                parNombre.SqlDbType = SqlDbType.VarChar;
+                parNombre.Size = 50;
+                //metodo get obtiene el metodo Nombre
+                parNombre.Value = Presentacion.Nombre;
+                sqlcmd.Parameters.Add(parNombre);
+                //descripcion
+                SqlParameter parDescripcion = new SqlParameter();
+                parDescripcion.ParameterName = "@descripcion";
+                parDescripcion.SqlDbType = SqlDbType.VarChar;
+                parDescripcion.Size = 256;
+                //metodo get obtiene el metodo Descrpcion
+                parDescripcion.Value = Presentacion.Descripcion;
+                sqlcmd.Parameters.Add(parDescripcion);
+
+                //ejecutamos nuestro comando
+                rpta = sqlcmd.ExecuteNonQuery() == 1 ? "Ok" : "No se actualizo el registro";
+            }
+            catch (Exception ex)
+            {
+                rpta = ex.Message;
+            }
+            finally
+            {
+                if (sqlcon.State == ConnectionState.Open) sqlcon.Close();
+            }
+            return rpta;
+        }
+        //Metodo Eliminar
+        public string Eliminar(DPresentacion Presentacion)
+        {
+            string rpta = "";
+            SqlConnection sqlcon = new SqlConnection();
+            try
+            {
+                //establecer la cadena de conexion y abrirla
+                sqlcon.ConnectionString = Conexion.cn;
+                sqlcon.Open();
+                //establecer el comando para ejecutar sentecias sql
+                SqlCommand sqlcmd = new SqlCommand();
+                sqlcmd.Connection = sqlcon;
+                sqlcmd.CommandText = "speliminar_presentacion";
+                sqlcmd.CommandType = CommandType.StoredProcedure;
+
+                //declarar parametros q recibe el procedimiento almacenado
+                SqlParameter parIdpresentacion = new SqlParameter();
+                parIdpresentacion.ParameterName = "@idpresentacion";
+                parIdpresentacion.SqlDbType = SqlDbType.Int;
+                //parametro de salida por ser autonumerico
+                parIdpresentacion.Value = Presentacion.Idpresentacion;
+                sqlcmd.Parameters.Add(parIdpresentacion);
+
+                //ejecutamos nuestro comando
+                rpta = sqlcmd.ExecuteNonQuery() == 1 ? "Ok" : "No se elimino el registro";
+            }
+            catch (Exception ex)
+            {
+                rpta = ex.Message;
+            }
+            finally
+            {
+                if (sqlcon.State == ConnectionState.Open) sqlcon.Close();
+            }
+            return rpta;
+        }
+        //Metodo Mostrar
+        public DataTable Mostrar()
+        {
+            //envio como parametro el nombre de la tabla
+            DataTable dtresultado = new DataTable("presentacion");
+            SqlConnection sqlcon = new SqlConnection();
+            try
+            {
+                //establesco la cadena de conexion
+                sqlcon.ConnectionString = Conexion.cn;
+                // sqlcon.Open();
+                //establecer el comando para ejecutar sentecias sql
+                SqlCommand sqlcmd = new SqlCommand();
+                sqlcmd.Connection = sqlcon;//indico al comando q cadena de conex va a usar
+                sqlcmd.CommandText = "spmostrar_presentacion";
+                sqlcmd.CommandType = CommandType.StoredProcedure;
+
+                //ejecuto el comando y lleno el datatable
+                SqlDataAdapter sqldat = new SqlDataAdapter(sqlcmd);
+                //rellena el adaptador con mi datatable
+                sqldat.Fill(dtresultado);
+            }
+            catch (Exception ex)
+            {
+                dtresultado = null;
+            }
+            //finally
+            //{
+            //    if (sqlcon.State == ConnectionState.Open) sqlcon.Close();
+            //}
+            return dtresultado;
+        }
+        //Metodo Buscar nombre
+        public DataTable BuscarNombre(DPresentacion Presentacion)
+        {
+            //envio como parametro el nombre de la tabla
+            DataTable dtresultado = new DataTable("presentacion");
+            SqlConnection sqlcon = new SqlConnection();
+            try
+            {
+                //establesco la cadena de conexion
+                sqlcon.ConnectionString = Conexion.cn;
+                //establecer el comando para ejecutar sentecias sql
+                SqlCommand sqlcmd = new SqlCommand();
+                sqlcmd.Connection = sqlcon;
+                sqlcmd.CommandText = "spbuscar_presentacion";
+                sqlcmd.CommandType = CommandType.StoredProcedure;
+
+                //parametro buscar
+                SqlParameter parTextoBuscar = new SqlParameter();
+                parTextoBuscar.ParameterName = "@textobuscar";
+                parTextoBuscar.SqlDbType = SqlDbType.VarChar;
+                parTextoBuscar.Size = 50;
+                //metodo get obtiene el metodo texto buscar
+                parTextoBuscar.Value = Presentacion.TextoBuscar;
+                sqlcmd.Parameters.Add(parTextoBuscar);
+
+                //ejecuto el comando y lleno el datatable
+                SqlDataAdapter sqldat = new SqlDataAdapter(sqlcmd);
+                //rellena el adaptador con mi datatable
+                sqldat.Fill(dtresultado);
+            }
+            catch (Exception ex)
+            {
+                dtresultado = null;
+            }
+            return dtresultado;
+        }
+    }
+}
---
+++ Negocio/NPresentacion.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using para acceder a datos
+using Datos;
+using System.Data;
+
+namespace Negocio
+{
+    //se agrega public para acceder desde presentacion
+    public class NPresentacion
+    {
+        //metodo insertar que llama a insertar de dcategoria en datos
+        public static string Insertar(string nombre, string descripcion)
+        {
+            DPresentacion obj = new DPresentacion();
+            obj.Nombre = nombre;
+            obj.Descripcion = descripcion;
+            return obj.Insertar(obj);
+        }
+        //editar
+        public static string Editar(int idpresentacion, string nombre, string descripcion)
+        {
+            DPresentacion obj = new DPresentacion();
+            obj.Idpresentacion = idpresentacion;
+            obj.Nombre = nombre;
+            obj.Descripcion = descripcion;
+            return obj.Editar(obj);
+        }
+        //eliminar
+        public static string Eliminar(int idpresentacion)
+        {
+            DPresentacion obj = new DPresentacion();
+            obj.Idpresentacion = idpresentacion;
+            return obj.Eliminar(obj);
+        }
+        //buscar
+        public static DataTable Buscar(string textobuscar)
+        {
+            DPresentacion obj = new DPresentacion();
+            obj.TextoBuscar = textobuscar;
+            return obj.BuscarNombre(obj);
+        }
+        //mostrar        
+        public static DataTable Mostrar()
+        {
+            return new DPresentacion().Mostrar();
+        }
+    }
+}
