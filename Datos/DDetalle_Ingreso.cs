e9f2b3605cc3368068c377d347c5ada0dc53bf0f
Pc <coli@tobynson.com>
2019-05-17 08:33:53 -0500

    se finalizo dingreso y ddetalle_ingreso

---
+++ Datos/DDetalle_Ingreso.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//usings necesarios para trabajar con sql
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Datos
+{
+    //hacerlo public para acceder desde negocio
+    public class DDetalle_Ingreso
+    {
+        private int _Iddetalle_ingreso;
+        private int _Idingreso;
+        private int _Idarticulo;
+        private decimal _Precio_compra;
+        private decimal _Precio_venta;
+        private int _Stock_inicial;
+        private int _Stock_actual;
+        private DateTime _Fecha_produccion;
+        private DateTime _Fecha_vencimiento;
+
+        public int Iddetalle_ingreso { get => _Iddetalle_ingreso; set => _Iddetalle_ingreso = value; }
+        public int Idingreso { get => _Idingreso; set => _Idingreso = value; }
+        public int Idarticulo { get => _Idarticulo; set => _Idarticulo = value; }
+        public decimal Precio_compra { get => _Precio_compra; set => _Precio_compra = value; }
+        public decimal Precio_venta { get => _Precio_venta; set => _Precio_venta = value; }
+        public int Stock_inicial { get => _Stock_inicial; set => _Stock_inicial = value; }
+        public int Stock_actual { get => _Stock_actual; set => _Stock_actual = value; }
+        public DateTime Fecha_produccion { get => _Fecha_produccion; set => _Fecha_produccion = value; }
+        public DateTime Fecha_vencimiento { get => _Fecha_vencimiento; set => _Fecha_vencimiento = value; }
+
+        public DDetalle_Ingreso()
+        {
+        }
+
+        public DDetalle_Ingreso(int iddetalle_ingreso, int idingreso, int idarticulo, decimal precio_compra, decimal precio_venta, int stock_inicial, int stock_actual, DateTime fecha_produccion, DateTime fecha_vencimiento)
+        {
+            this.Iddetalle_ingreso = iddetalle_ingreso;
+            this.Idingreso = idingreso;
+            this.Idarticulo = idarticulo;
+            this.Precio_compra = precio_compra;
+            this.Precio_venta = precio_venta;
+            this.Stock_inicial = stock_inicial;
+            this.Stock_actual = stock_actual;
+            this.Fecha_produccion = fecha_produccion;
+            this.Fecha_vencimiento = fecha_vencimiento;
+        }
+        //Metodo Insertar (recibe la coneccion de Dingreso y la transaccion por referencia)
+        public string Insertar(DDetalle_Ingreso Detalle_Ingreso,ref SqlConnection sqlcon,ref SqlTransaction sqltra)
+        {
+            //la coneccion ya la recibo con el parametro sqlcon sqltra un ingreso con una sola trnasaccion
+            string rpta = "";
+            
+            try
+            {                
+                //sqlcon.Open();
+                //establecer el comando para ejecutar sentecias sql
+                SqlCommand sqlcmd = new SqlCommand();
+                sqlcmd.Connection = sqlcon;
+                //===============agregado para la transaccion============
+                sqlcmd.Transaction = sqltra;
+                //=======================================================
+                sqlcmd.CommandText = "spinsertar_detalle_ingreso";
+                sqlcmd.CommandType = CommandType.StoredProcedure;
+
+                //declarar parametros q recibe el procedimiento almacenado
+                SqlParameter parIddetalle_ingreso = new SqlParameter();
+                parIddetalle_ingreso.ParameterName = "@iddetalle_ingreso";
+                parIddetalle_ingreso.SqlDbType = SqlDbType.Int;
+                //parametro de salida por ser autonumerico
+                parIddetalle_ingreso.Direction = ParameterDirection.Output;
+                sqlcmd.Parameters.Add(parIddetalle_ingreso);
+                //idingreso
+                SqlParameter parIdingreso = new SqlParameter();
+                parIdingreso.ParameterName = "@idingreso";
+                parIdingreso.SqlDbType = SqlDbType.Int;
+                parIdingreso.Value = Detalle_Ingreso.Idingreso;
+                //metodo get obtiene el metodo Nombre                
+                sqlcmd.Parameters.Add(parIdingreso);
+                //idarticulo
+                SqlParameter parIdarticulo = new SqlParameter();
+                parIdarticulo.ParameterName = "@idarticulo";
+                parIdarticulo.SqlDbType = SqlDbType.Int;
+                parIdarticulo.Value = Detalle_Ingreso.Idarticulo;
+                //metodo get obtiene el metodo Descrpcion                
+                sqlcmd.Parameters.Add(parIdarticulo);
+                //precio compra
+                SqlParameter parPrecio_compra = new SqlParameter();
+                parPrecio_compra.ParameterName = "@precio_compra";
+                parPrecio_compra.SqlDbType = SqlDbType.Money;
+                parPrecio_compra.Value = Detalle_Ingreso.Precio_compra;
+                //metodo get obtiene el metodo Descrpcion                
+                sqlcmd.Parameters.Add(parPrecio_compra);
+                //precio venta
+                SqlParameter parPrecio_venta = new SqlParameter();
+                parPrecio_venta.ParameterName = "@precio_venta";
+                parPrecio_venta.SqlDbType = SqlDbType.Money;
+                parPrecio_venta.Value = Detalle_Ingreso.Precio_venta;
+                //metodo get obtiene el metodo Descrpcion                
+                sqlcmd.Parameters.Add(parPrecio_venta);
+                //stock inicial
+                SqlParameter parStock_inicial = new SqlParameter();
+                parStock_inicial.ParameterName = "@stock_inicial";
+                parStock_inicial.SqlDbType = SqlDbType.Int;
+                parStock_inicial.Value = Detalle_Ingreso.Stock_inicial;
+                //metodo get obtiene el metodo Descrpcion                
+                sqlcmd.Parameters.Add(parStock_inicial);
+                //stock actual
+                SqlParameter parStock_actual = new SqlParameter();
+                parStock_actual.ParameterName = "@stock_actual";
+                parStock_actual.SqlDbType = SqlDbType.Int;
+                parStock_actual.Value = Detalle_Ingreso.Stock_actual;
+                //metodo get obtiene el metodo Descrpcion                
+                sqlcmd.Parameters.Add(parStock_actual);
+                //fecha produccion
+                SqlParameter parFecha_produccion = new SqlParameter();
+                parFecha_produccion.ParameterName = "@fecha_produccion";
+                parFecha_produccion.SqlDbType = SqlDbType.DateTime;
+                parFecha_produccion.Value = Detalle_Ingreso.Fecha_produccion;
+                //metodo get obtiene el metodo Descrpcion                
+                sqlcmd.Parameters.Add(parFecha_produccion);
+                //fecha vencimiento
+                SqlParameter parFecha_vencimiento = new SqlParameter();
+                parFecha_vencimiento.ParameterName = "@fecha_vencimiento";
+                parFecha_vencimiento.SqlDbType = SqlDbType.DateTime;
+                parFecha_vencimiento.Value = Detalle_Ingreso.Fecha_vencimiento;
+                //metodo get obtiene el metodo Descrpcion                
+                sqlcmd.Parameters.Add(parFecha_vencimiento);
+
+                //ejecutamos nuestro comando
+                rpta = sqlcmd.ExecuteNonQuery() == 1 ? "Ok" : "No se ingreso el registro";
+            }
+            catch (Exception ex)
+            {
+                rpta = ex.Message;
+            }
+            //no se cierra para seguir insertando otros detalle de ingreso xq 1 ingreso tiene 1 o varios detalles
+            return rpta;
+        }
+    }
+}
---
+++ Datos/DIngreso.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//usings necesarios para trabajar con sql
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Datos
+{
+    class DIngreso
+    {
+        private int _Idingreso;
+        private int _Idtrabajador;
+        private int _Idproveedor;
+        private DateTime _Fecha;
+        private string _Tipo_comprobante;
+        private string _Serie;
+        private string _Correlativo;
+        private decimal _Igv;
+        private string _Estado;
+
+        public int Idingreso { get => _Idingreso; set => _Idingreso = value; }
+        public int Idtrabajador { get => _Idtrabajador; set => _Idtrabajador = value; }
+        public int Idproveedor { get => _Idproveedor; set => _Idproveedor = value; }
+        public DateTime Fecha { get => _Fecha; set => _Fecha = value; }
+        public string Tipo_comprobante { get => _Tipo_comprobante; set => _Tipo_comprobante = value; }
+        public string Serie { get => _Serie; set => _Serie = value; }
+        public string Correlativo { get => _Correlativo; set => _Correlativo = value; }
+        public decimal Igv { get => _Igv; set => _Igv = value; }
+        public string Estado { get => _Estado; set => _Estado = value; }
+
+        public DIngreso()
+        {
+        }
+
+        public DIngreso(int idingreso, int idtrabajador, int idproveedor, DateTime fecha, string tipo_comprobante, string serie, string correlativo, decimal igv, string estado)
+        {
+            this.Idingreso = idingreso;
+            this.Idtrabajador = idtrabajador;
+            this.Idproveedor = idproveedor;
+            this.Fecha = fecha;
+            this.Tipo_comprobante = tipo_comprobante;
+            this.Serie = serie;
+            this.Correlativo = correlativo;
+            this.Igv = igv;
+            this.Estado = estado;
+        }
+        //Metodo Insertar (ingreso y todos sus detalles a modo lista)
+        public string Insertar(DIngreso Ingreso,List<DDetalle_Ingreso> Detalle)
+        {
+            string rpta = "";
+            SqlConnection sqlcon = new SqlConnection();
+            try
+            {
+                //establecer la cadena de conexion y abrirla
+                sqlcon.ConnectionString = Conexion.cn;
+                sqlcon.Open();
+                //=============INICIO DE TRANSACCION==============
+                    //cada detalle ingreso se realacione con un solo ingreso
+                SqlTransaction sqltra = sqlcon.BeginTransaction();                              
+                SqlCommand sqlcmd = new SqlCommand();
+                sqlcmd.Connection = sqlcon;
+                    //le decimos al comando que trabaje con esta transaccion
+                sqlcmd.Transaction = sqltra; 
+                //================================================  
+                sqlcmd.CommandText = "spinsertar_ingreso";
+                sqlcmd.CommandType = CommandType.StoredProcedure;
+
+                //declarar parametros q recibe el procedimiento almacenado
+                SqlParameter parIdingreso = new SqlParameter();
+                parIdingreso.ParameterName = "@idingreso";
+                parIdingreso.SqlDbType = SqlDbType.Int;
+                //parametro de salida por ser autonumerico
+                parIdingreso.Direction = ParameterDirection.Output;
+                sqlcmd.Parameters.Add(parIdingreso);
+                //idtrabajador
+                SqlParameter parIdtrabajador = new SqlParameter();
+                parIdtrabajador.ParameterName = "@idtrabajador";
+                parIdtrabajador.SqlDbType = SqlDbType.Int;                
+                //metodo get obtiene el metodo Nombre
+                parIdtrabajador.Value = Ingreso.Idtrabajador;
+                sqlcmd.Parameters.Add(parIdtrabajador);
+                //idproveedor
+                SqlParameter parIdproveedor = new SqlParameter();
+                parIdproveedor.ParameterName = "@idproveedor";
+                parIdproveedor.SqlDbType = SqlDbType.Int;
+                //metodo get obtiene el metodo Descrpcion
+                parIdproveedor.Value = Ingreso.Idproveedor;
+                sqlcmd.Parameters.Add(parIdproveedor);
+                //fecha
+                SqlParameter parFecha = new SqlParameter();
+                parFecha.ParameterName = "@fecha";
+                parFecha.SqlDbType = SqlDbType.DateTime;
+                //metodo get obtiene el metodo Descrpcion
+                parFecha.Value = Ingreso.Fecha;
+                sqlcmd.Parameters.Add(parFecha);
+                //tipo comprobante
+                SqlParameter parTipo_comprobante = new SqlParameter();
+                parTipo_comprobante.ParameterName = "@tipo_comprobante";
+                parTipo_comprobante.SqlDbType = SqlDbType.VarChar;
+                parTipo_comprobante.Size = 20;
+                //metodo get obtiene el metodo Descrpcion
+                parTipo_comprobante.Value = Ingreso.Tipo_comprobante;
+                sqlcmd.Parameters.Add(parTipo_comprobante);
+                //serie
+                SqlParameter parSerie = new SqlParameter();
+                parSerie.ParameterName = "@serie";
+                parSerie.SqlDbType = SqlDbType.VarChar;
+                parSerie.Size = 4;
+                //metodo get obtiene el metodo Descrpcion
+                parSerie.Value = Ingreso.Serie;
+                sqlcmd.Parameters.Add(parSerie);
+                //correlativo
+                SqlParameter parCorrelativo = new SqlParameter();
+                parCorrelativo.ParameterName = "@correlativo";
+                parCorrelativo.SqlDbType = SqlDbType.VarChar;
+                parCorrelativo.Size = 7;
+                //metodo get obtiene el metodo Descrpcion
+                parCorrelativo.Value = Ingreso.Correlativo;
+                sqlcmd.Parameters.Add(parCorrelativo);
+                //igv
+                SqlParameter parIgv = new SqlParameter();
+                parIgv.ParameterName = "@igv";
+                parIgv.SqlDbType = SqlDbType.Decimal;//tiene presicion y scala
+                parIgv.Precision = 4;
+                parIgv.Scale = 2;
+                //metodo get obtiene el metodo Descrpcion
+                parIgv.Value = Ingreso.Igv;
+                sqlcmd.Parameters.Add(parIgv);
+                //estado
+                SqlParameter parEstado = new SqlParameter();
+                parEstado.ParameterName = "@estado";
+                parEstado.SqlDbType = SqlDbType.VarChar;
+                parEstado.Size = 7;
+                //metodo get obtiene el metodo Descrpcion
+                parEstado.Value = Ingreso.Estado;
+                sqlcmd.Parameters.Add(parEstado);
+
+                //ejecutamos nuestro comando
+                rpta = sqlcmd.ExecuteNonQuery() == 1 ? "Ok" : "No se ingreso el registro";
+                //si es ok Dingreso inserto los detalles_ingreso
+                if (rpta.Equals("Ok"))
+                {
+                    //obtenemos el idingreso de la tabla ingreso para detalle_ingreso
+                    this.Idingreso = Convert.ToInt32(sqlcmd.Parameters["@idingreso"].Value);//value por ser de salida
+                    //recorre la lista Detalle en la var det del mismo tipo q la lista
+                    foreach (DDetalle_Ingreso det in Detalle)
+                    {
+                        det.Idingreso = this.Idingreso; //envio el idingreso obtenido a detalle_ingreso
+                        //insertar de la clase detalle_ingreso
+                        rpta = det.Insertar(det,ref sqlcon,ref sqltra);
+                        if (!rpta.Equals("Ok")) //si no se pudo insertar
+                        {
+                            break;// salgo del bucle
+                        }
+                    }
+                }
+                //si se pudo insertar
+                if (rpta.Equals("Ok"))
+                {
+                    sqltra.Commit();//confirmo la transaccion
+                }
+                else
+                {
+                    sqltra.Rollback();//se niega la transaccion
+                }
+
+            }
+            catch (Exception ex)
+            {
+                rpta = ex.Message;
+            }
+            finally
+            {
+                if (sqlcon.State == ConnectionState.Open) sqlcon.Close();
+            }
+            return rpta;
+        }        
+        public string Anular(DIngreso Ingreso)
+        {
+            string rpta = "";
+            SqlConnection sqlcon = new SqlConnection();
+            try
+            {
+                //establecer la cadena de conexion y abrirla
+                sqlcon.ConnectionString = Conexion.cn;
+                sqlcon.Open();
+                //establecer el comando para ejecutar sentecias sql
+                SqlCommand sqlcmd = new SqlCommand();
+                sqlcmd.Connection = sqlcon;
+                sqlcmd.CommandText = "spanular_ingreso";
+                sqlcmd.CommandType = CommandType.StoredProcedure;
+
+                //declarar parametros q recibe el procedimiento almacenado
+                SqlParameter parIdingreso = new SqlParameter();
+                parIdingreso.ParameterName = "@idingreso";
+                parIdingreso.SqlDbType = SqlDbType.Int;
+                //parametro de salida por ser autonumerico
+                parIdingreso.Value = Ingreso.Idingreso;
+                sqlcmd.Parameters.Add(parIdingreso);
+
+                //ejecutamos nuestro comando
+                rpta = sqlcmd.ExecuteNonQuery() == 1 ? "Ok" : "No se eanulo el registro";
+            }
+            catch (Exception ex)
+            {
+                rpta = ex.Message;
+            }
+            finally
+            {
+                if (sqlcon.State == ConnectionState.Open) sqlcon.Close();
+            }
+            return rpta;
+        }
+        //Metodo Mostrar
+        public DataTable Mostrar()
+        {
+            //envio como parametro el nombre de la tabla
+            DataTable dtresultado = new DataTable("ingreso");
+            SqlConnection sqlcon = new SqlConnection();
+            try
+            {
+                //establesco la cadena de conexion
+                sqlcon.ConnectionString = Conexion.cn;
+                // sqlcon.Open();
+                //establecer el comando para ejecutar sentecias sql
+                SqlCommand sqlcmd = new SqlCommand();
+                sqlcmd.Connection = sqlcon;//indico al comando q cadena de conex va a usar
+                sqlcmd.CommandText = "spmostrar_ingreso";
+                sqlcmd.CommandType = CommandType.StoredProcedure;
+
+                //ejecuto el comando y lleno el datatable
+                SqlDataAdapter sqldat = new SqlDataAdapter(sqlcmd);
+                //rellena el adaptador con mi datatable
+                sqldat.Fill(dtresultado);
+            }
+            catch (Exception ex)
+            {
+                dtresultado = null;
+            }
+            //finally
+            //{
+            //    if (sqlcon.State == ConnectionState.Open) sqlcon.Close();
+            //}
+            return dtresultado;
+        }
+        //Metodo Buscar fechas
+        public DataTable BuscarFechas(string textoBuscar,string textoBuscar2)
+        {
+            //envio como parametro el nombre de la tabla
+            DataTable dtresultado = new DataTable("ingreso");
+            SqlConnection sqlcon = new SqlConnection();
+            try
+            {
+                //establesco la cadena de conexion
+                sqlcon.ConnectionString = Conexion.cn;
+                //establecer el comando para ejecutar sentecias sql
+                SqlCommand sqlcmd = new SqlCommand();
+                sqlcmd.Connection = sqlcon;
+                sqlcmd.CommandText = "spbuscar_ingreso_fecha";
+                sqlcmd.CommandType = CommandType.StoredProcedure;
+
+                //parametro buscar
+                SqlParameter parTextoBuscar = new SqlParameter();
+                parTextoBuscar.ParameterName = "@textobuscar";
+                parTextoBuscar.SqlDbType = SqlDbType.VarChar;
+                parTextoBuscar.Size = 50;
+                //metodo get obtiene el metodo texto buscar
+                parTextoBuscar.Value = textoBuscar;
+                sqlcmd.Parameters.Add(parTextoBuscar);
+                //parametro buscar2
+                SqlParameter parTextoBuscar2 = new SqlParameter();
+                parTextoBuscar2.ParameterName = "@textobuscar2";
+                parTextoBuscar2.SqlDbType = SqlDbType.VarChar;
+                parTextoBuscar2.Size = 50;
+                //metodo get obtiene el metodo texto buscar
+                parTextoBuscar2.Value = textoBuscar2;
+                sqlcmd.Parameters.Add(parTextoBuscar2);
+
+                //ejecuto el comando y lleno el datatable
+                SqlDataAdapter sqldat = new SqlDataAdapter(sqlcmd);
+                //rellena el adaptador con mi datatable
+                sqldat.Fill(dtresultado);
+            }
+            catch (Exception ex)
+            {
+                dtresultado = null;
+            }
+            return dtresultado;
+        }
+        //mostrar detalle
+        public DataTable mostrarDetalle(string textoBuscar)
+        {
+            //envio como parametro el nombre de la tabla
+            DataTable dtresultado = new DataTable("detalle_ingreso");
+            SqlConnection sqlcon = new SqlConnection();
+            try
+            {
+                //establesco la cadena de conexion
+                sqlcon.ConnectionString = Conexion.cn;
+                //establecer el comando para ejecutar sentecias sql
+                SqlCommand sqlcmd = new SqlCommand();
+                sqlcmd.Connection = sqlcon;
+                sqlcmd.CommandText = "spmostrar_detalle_ingreso";
+                sqlcmd.CommandType = CommandType.StoredProcedure;
+
+                //parametro buscar
+                SqlParameter parTextoBuscar = new SqlParameter();
+                parTextoBuscar.ParameterName = "@textobuscar";
+                parTextoBuscar.SqlDbType = SqlDbType.VarChar;
+                parTextoBuscar.Size = 50;
+                //metodo get obtiene el metodo texto buscar
+                parTextoBuscar.Value = textoBuscar;
+                sqlcmd.Parameters.Add(parTextoBuscar);               
+
+                //ejecuto el comando y lleno el datatable
+                SqlDataAdapter sqldat = new SqlDataAdapter(sqlcmd);
+                //rellena el adaptador con mi datatable
+                sqldat.Fill(dtresultado);
+            }
+            catch (Exception ex)
+            {
+                dtresultado = null;
+            }
+            return dtresultado;
+        }
+    }
+}
